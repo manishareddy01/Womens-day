7481c6dc65f9798006fa8a2973b168fdee35a737
unknown <manishaaddula@gmail.com>
2024-03-11 14:06:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import "./puzzle.css";
+import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+import img from "./pic2.png";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("Unpuzzle the pieces!!");
+     
+    const set = () => {
+        setText("Congratulations!!");
+    };
+     
+    return (
+        <div className=" bg-lavender h-screen font-poppins flex flex-col">
+            <h2 className="text-3xl justify-center items-center flex p-6 mt-4">{text}</h2>
+            <div className="w-full max-w-lg md:w-210 md:h-200">
+              </div><JigsawPuzzle 
+                imageSrc={img}
+                rows={3}
+                columns={3}
+                onSolved={set}
+                className="jigsaw-puzzle "
+            />
+        </div>
+    );
 }
-
+ 
 export default App;
